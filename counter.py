d9d0b0766bddd6062f39cedd03e0b5754fd4d89a
gokayozcoban <43710003+gokayozcoban@users.noreply.github.com>
2019-01-06 21:18:32 +0200

    counter

---
+++ counter.py
@@ -0,0 +1,35 @@
+import time
+for i in range(10):
+    print(i)
+    time.sleep(1)
+
+#BİR SANİYE ARAYLA range() PARANTEZİNDE BELİRTİLEN SAYIYI EKRANA YAZAR.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
